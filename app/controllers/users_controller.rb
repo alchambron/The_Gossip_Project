f6012b272d03a52d45422f86d91e58f1f3a4e953
alchambron <alexis.chambron@gmail.com>
2022-11-03 16:01:33 +0100

    Adding User Creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,30 @@ class UsersController < ApplicationController
     @user_db = User.find_by(id: params[:id])
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+  @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      description: params[:description],
+      email: params[:email],
+      age: params[:age],
+      password: params[:password],
+      password_confirmation: params[:confirm_password],
+      city_id: rand(1..10)
+    )
+
+  if @user.save
+    puts 'it workkksss'
+    redirect_to '/'
+  else
+    puts 'noooooo'
+    render new_user_path
+  end
+  end
 
   def edit; end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+
   belongs_to :city
   has_many :gossips
   has_many :comments
+
+  has_secure_password
 end
